c8e6a1c3f263b6e88fc0d609d3671f703c8e5259
李翌辰 <lanxan0309@gmail.com>
2014-12-31 17:52:44 +0800

    Update and rename chapter_10.hs to chapter10.hs
    領悟 單一 pattern 的用法

--- chapter10.hs
+++ chapter10.hs
@@ -4,6 +4,12 @@ _map' mapper lt =
     if null lt then lt
     else (mapper $ head lt) : (_map' mapper $ tail lt)
     
+_length' :: [a] -> Int
+_length' lt =
+    if null lt then 0
+    else 1 + (_length' $ tail lt)
+
+        
 --pattern matching
 map' :: (Int -> Int) -> [Int] -> [Int]
 map' _ [] = []
@@ -12,8 +18,18 @@ map' mapper (x:xs) =
     else (mapper x) : (map' mapper xs)
 --or
 --  map' mapper (x:xs) = mapper x : (map' mapper xs) 
+length' :: [a] -> Int
+length' [] = 0
+length' (x:xs) = 1 + (length' xs) 
+
+sum' :: [Int] -> Int
+sum' [] = 0
+sum' (x:xs) = x + (sum' xs)
     
-    
+
+
 --print
 main = do
-  putStrLn $ show $ map' (+5) [1,2,3,4]
+    putStrLn $ show $ map' (+5) [1,2,3,4] -- [6,7,8,9]
+    putStrLn $ show $ length' [1,2,3,4] -- 4
+    putStrLn $ show $ sum' [1,2,3,4] -- 10
